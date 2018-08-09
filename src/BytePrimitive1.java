36c5df7297a40e58e5bb888be72b5b90e9dd94b3
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-08 22:38:31 -0500

    examples of byte primitives

--- src/BytePrimitive1.java
+++ src/BytePrimitive1.java
@@ -9,9 +9,18 @@ public class BytePrimitive1 {
         byte b3 = 0x19; // Hexadecimal
         byte b4 = 031; // Octal
 
+
         System.out.println("b1 = " + b1);
         System.out.println("b2 = " + b2);
         System.out.println("b3 = " + b3);
         System.out.println("b4 = " + b4);
+
+//        byte b5 = -129; // will not compile due to the out of range
+
+//        byte b6 = 128; // will not compile due to the out of range
+//        // b5&b6 will say an error: possible lossy conversion from int to byte
+//        System.out.println("b5 = " + b5);
+//        System.out.println("b6 = " + b6);
+
     }
 }
