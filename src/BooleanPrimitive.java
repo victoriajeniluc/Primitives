c6cb248972f2573aa31887654b40363403a2c703
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-09 01:09:01 -0500

    examples of boolean primitives

---
+++ src/BooleanPrimitive.java
@@ -0,0 +1,12 @@
+/**
+ * Class to show boolean primitives
+ */
+public class BooleanPrimitive {
+    public static void main(String[] args) {
+        boolean status = true;
+        System.out.println("Eligible to appear for the exam? " + status);
+
+        // boolean b1 = 0; will throw an error
+        // boolean b2 = "true"; will throw an error
+    }
+}
